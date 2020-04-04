0ceabe3a97dc08c7c19dc8723643e75cf5f0f6b4
birukbelay <belay.bura@gmail.com>
2020-04-04 22:38:50 +0300

    13-8 before refactoring the auth method to make login and sign up to have the same logic
    Took 2 hours 52 minutes

--- lib/pages/auth.dart
+++ lib/pages/auth.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:scoped_model/scoped_model.dart';
+
 //my
 import '../scoped_models/main_model.dart';
 //import 'package:academia1/pages/products_page.dart';
+
+enum AuthMode { SignUp, Login }
 
 class AuthPage extends StatefulWidget {
   @override
@@ -12,24 +15,29 @@ class AuthPage extends StatefulWidget {
 }
 
 class _AuthPage extends State<AuthPage> {
-  final Map<String, dynamic> _formData ={
-  'email' : null,
-  'password' : null,
-  'acceptTerms' : false,
+  final Map<String, dynamic> _formData = {
+    'email': null,
+    'password': null,
+    'acceptTerms': false,
   };
 
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final TextEditingController _passwordTextController = TextEditingController();
+  AuthMode _authMode = AuthMode.Login;
 
+//  ================================Widgets=====================================
+//==============================================================================
 
-// =========================  The decoration image for the background  ================
+// ================   The decoration image for the background  ===
   DecorationImage decorationImageWidget() {
     return DecorationImage(
         fit: BoxFit.cover,
         colorFilter:
-        ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
+            ColorFilter.mode(Colors.black.withOpacity(0.3), BlendMode.dstATop),
         image: AssetImage('assets/images/food/9.jpg'));
   }
-//   =========================  the Email Widget  ======================
+
+//   ================   the Email Widget  ================
   Widget emailWidget() {
     return TextFormField(
       decoration: InputDecoration(
@@ -41,6 +49,7 @@ class _AuthPage extends State<AuthPage> {
                 .hasMatch(value)) {
           return 'Please enter a valid email';
         }
+        return null;
       },
       onSaved: (String value) {
         _formData['email'] = value;
@@ -50,22 +59,46 @@ class _AuthPage extends State<AuthPage> {
 
 //  ==================  The password Widget  ====================
 
-   Widget passwordWidget() {
+  Widget passwordWidget() {
     return TextFormField(
       decoration: InputDecoration(
           labelText: 'Password', filled: true, fillColor: Colors.white12),
       obscureText: true,
+      controller: _passwordTextController,
       validator: (String value) {
         if (value.isEmpty || value.length < 6) {
           return 'Password invalid';
         }
+        return null;
       },
       onSaved: (String value) {
         _formData['password'] = value;
       },
     );
   }
-//=================== accceot switch==========
+
+//    =============== password confirm ==========
+
+  Widget _passwordConfirmWidget() {
+    return TextFormField(
+      decoration: InputDecoration(
+          labelText: 'conferm  Password',
+          filled: true,
+          fillColor: Colors.white12),
+      obscureText: true,
+      validator: (String value) {
+        if (_passwordTextController.text != value) {
+          return 'Password do not match';
+        }
+        return null;
+      },
+      onSaved: (String value) {
+        _formData['password'] = value;
+      },
+    );
+  }
+
+//=================== accept switch==========
   Widget _buildAcceptSwitch() {
     return SwitchListTile(
       value: _formData['acceptTerms'],
@@ -77,6 +110,7 @@ class _AuthPage extends State<AuthPage> {
       title: Text('Accept Terms'),
     );
   }
+
 //======================  ApppBar Widget  =====================
   Widget _appbar() {
     return AppBar(
@@ -84,21 +118,48 @@ class _AuthPage extends State<AuthPage> {
     );
   }
 
+//=================================Functions====================================
+//  ============================================================================
 
-
-  void _submitForm(Function login) {
+  void _submitForm(Function login, Function signup) async {
     if (!_formKey.currentState.validate() || !_formData['acceptTerms']) {
       return;
     }
     _formKey.currentState.save();
-    login(_formData['email'], _formData['password']);
-    Navigator.pushReplacementNamed(context, '/admin');
+    Map<String, dynamic> succesInformation;
+    if (_authMode == AuthMode.Login) {
+      succesInformation =
+          await login(_formData['email'], _formData['password']);
+    } else {
+      succesInformation =
+          await signup(_formData['email'], _formData['password']);
+    }
+
+    if (succesInformation['sucess']) {
+      Navigator.pushReplacementNamed(context, '/admin');
+    } else {
+      showDialog(
+          context: context,
+          builder: (context) {
+            return AlertDialog(
+              title: Text('an error has occored'),
+              content: Text(succesInformation['message']),
+              actions: <Widget>[
+                FlatButton(
+                  child: Text('Ok'),
+                  onPressed: () {
+                    Navigator.of(context).pop();
+                  },
+                )
+              ],
+            );
+          });
+    }
   }
 
 //  =====================  --- MAIN BUILD METHOD --- =================
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       appBar: _appbar(),
       body: Container(
@@ -117,14 +178,40 @@ class _AuthPage extends State<AuthPage> {
                     height: 10.0,
                   ),
                   passwordWidget(),
+                  _authMode == AuthMode.SignUp
+                      ? _passwordConfirmWidget()
+                      : Container(),
+                  SizedBox(
+                    height: 10.0,
+                  ),
                   _buildAcceptSwitch(),
-                  ScopedModelDescendant(builder: (BuildContext context, Widget child, MainModel model){
-                   return RaisedButton(
-                      child: Text('login'),
-                      onPressed: () =>_submitForm(model.login),
-                    );
-                  },
-
+                  FlatButton(
+                    child: Text(
+                        'switch to ${_authMode == AuthMode.Login ? 'Signup' : "Login"}'),
+                    onPressed: () {
+                      setState(() {
+                        _authMode = _authMode == AuthMode.Login
+                            ? AuthMode.SignUp
+                            : AuthMode.Login;
+                      });
+                    },
+                  ),
+                  SizedBox(
+                    height: 10.0,
+                  ),
+                  ScopedModelDescendant<MainModel>(
+                    builder:
+                        (BuildContext context, Widget child, MainModel model) {
+                      return model.isLoading
+                          ? CircularProgressIndicator()
+                          : RaisedButton(
+                              child: Text(_authMode == AuthMode.Login
+                                  ? 'Login'
+                                  : 'Signup'),
+                              onPressed: () =>
+                                  _submitForm(model.login, model.signup),
+                            );
+                    },
                   ),
                 ],
               ),
--- lib/scoped_models/connected_product.dart
+++ lib/scoped_models/connected_product.dart
@@ -18,54 +18,7 @@ class ConnectedProducts extends Model {
   String staticImage =
       'https://m.media-amazon.com/images/S/aplus-media/vc/7ee7f4e8-31f9-475e-b2b4-dbf30c1f0f11._CR189,0,1814,1814_PT0_SX300__.jpg';
 
-//  ===================   add product ===============
 
-  Future<bool> addProduct(
-      String title, String image, double price, String description) {
-    _isLoading = true;
-    notifyListeners();
-
-    final Map<String, dynamic> productData = {
-      'title': title,
-      'description': description,
-      'image': staticImage,
-      'price': price,
-      'userEmail': _authenticatedUser.email,
-      'userId': _authenticatedUser.id
-    };
-
-    return http
-        .post(_productUrl, body: json.encode(productData))
-        .then((http.Response response) {
-      if (response.statusCode != 200 || response.statusCode != 201) {
-        _isLoading = false;
-        notifyListeners();
-        return false;
-      }
-
-      final Map<String, dynamic> responseData = json.decode(response.body);
-
-      final Product newProduct = new Product(
-          id: responseData['name'],
-          title: title,
-          image: image,
-          price: price,
-          description: description,
-          userEmail: _authenticatedUser.email,
-          userId: _authenticatedUser.id);
-
-      _products.add(newProduct);
-      _selProductId = null;
-      _isLoading = false;
-      notifyListeners();
-      return true;
-    }).catchError((e) {
-      _isLoading = false;
-      notifyListeners();
-      print("error...: $e");
-      return false;
-    });
-  }
 }
 
 //######================== product model mixin ==================
@@ -152,6 +105,56 @@ mixin ProductsModel on ConnectedProducts {
     });
   } //fetch products
 
+  //  ===================   add product ===============
+
+  Future<bool> addProduct(
+      String title, String image, double price, String description) async {
+    _isLoading = true;
+    notifyListeners();
+
+    final Map<String, dynamic> productData = {
+      'title': title,
+      'description': description,
+      'image': staticImage,
+      'price': price,
+      'userEmail': _authenticatedUser.email,
+      'userId': _authenticatedUser.id
+    };
+
+    try {
+      final http.Response response =
+      await http.post(_productUrl, body: json.encode(productData));
+
+      if (response.statusCode != 200 || response.statusCode != 201) {
+        _isLoading = false;
+        notifyListeners();
+        return false;
+      }
+
+      final Map<String, dynamic> responseData = json.decode(response.body);
+
+      final Product newProduct = new Product(
+          id: responseData['name'],
+          title: title,
+          image: image,
+          price: price,
+          description: description,
+          userEmail: _authenticatedUser.email,
+          userId: _authenticatedUser.id);
+
+      _products.add(newProduct);
+      _selProductId = null;
+      _isLoading = false;
+      notifyListeners();
+      return true;
+    } catch (e) {
+      _isLoading = false;
+      notifyListeners();
+      print("error...: $e");
+      return false;
+    }
+  }
+
 //  ---------------------====2 updateProduct ====---------------------
 
   Future<bool> updateProduct(
@@ -192,7 +195,6 @@ mixin ProductsModel on ConnectedProducts {
       _selProductId = null;
       print("error...: $e");
       return false;
-
     });
   }
 
@@ -204,8 +206,9 @@ mixin ProductsModel on ConnectedProducts {
     return http
         .delete(_productUrl + '/$deletedId.json')
         .then((http.Response response) {
-      _isLoading = false;
+
       _products.removeAt(selectedProductIndex);
+      _isLoading = false;
       notifyListeners();
 //      _selProductIndex = null;
       return true;
@@ -216,7 +219,6 @@ mixin ProductsModel on ConnectedProducts {
       print("error...: $e");
       return false;
     });
-
   }
 
 //  ---------------  =======4 favToggle =====--------
@@ -247,18 +249,91 @@ mixin ProductsModel on ConnectedProducts {
 }
 
 //#####===========================  user model mixin ======================
+
 mixin UserModel on ConnectedProducts {
-  void login(String email, String password) {
-    _authenticatedUser = User(id: 'q123', email: email, password: password);
-    _isauthenticated = true;
+
+//  ----------------===== Login ========----------
+
+  Future<Map<String, dynamic>> login(String email, String password) async{
+    _isLoading=true;
     notifyListeners();
-  }
+    final Map<String, dynamic> authData ={
+      'email':email,
+      'password':password,
+      'returnSecureToken':true
+    };
+//    TODO  Login  backend needs to be set up
+    final http.Response response =await http.post('https:the uri?key=kldkflkjkj',body: json.encode(authData),
+    headers: {'Content-type': 'application/json'});
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    bool hasError =true;
+    String message = 'something went wrong';
+    if(responseData.containsKey('idToken')){
+      hasError=false;
+      message ='Authentication sucess';
+
+    }else if(responseData['error']['message']=='EMAIL_NOT_FOUND'){
+      message='EMAIL OR PASSWORD WRONG';
+    }
+    else if(responseData['error']['message']=='INVALID_PASSWORD'){
+      message='EMAIL OR PASSWORD WRONG';
+    }
+
+    print(response);
+    _isauthenticated = true;
+    _isLoading=false;
+    notifyListeners();
+
+//    @@@ CHANGED TO !HASERROR so that it will be fine in teh product edit page
+    return{'sucess': !hasError, 'message':message};
+
+
+//    _authenticatedUser = User(id: 'q123', email: email, password: password);
+//  _isauthenticated = true;
+
+}
 
   bool get isAuthenticated {
     return _isauthenticated;
   }
+
+//  =========--------signup------============
+
+  Future<Map<String, dynamic>> signup(String email, String password) async{
+    _isLoading=true;
+    notifyListeners();
+    final Map<String, dynamic> authData ={
+      'email':email,
+      'password':password,
+      'returnSecureToken':true
+    };
+    //  TODO SignUp back end not setup
+    final http.Response response =await http.post('https:my url with secure api',
+          body: json.encode(authData),
+      headers: {'Content-Type':'application/json'}
+    );
+    final Map<String, dynamic> responseData = json.decode(response.body);
+    bool hasError =true;
+    String message = 'something went wrong';
+    if(responseData.containsKey('idToken')){
+      hasError=false;
+      message ='Authentication sucess';
+
+    }else if(responseData['error']['message']=='EMAIL_EXISTS'){
+      message='this email alreaady exists';
+    }
+
+    print(response);
+    _isauthenticated = true;
+    _isLoading=true;
+    notifyListeners();
+//    @@@ CHANGED TO !HASERROR so that it will be fine in teh product edit page
+    return{'sucess': !hasError, 'message':message};
+
+  }
 }
 
+//========================== model utility ====================
 mixin UtilityModel on ConnectedProducts {
   bool get isLoading {
     return _isLoading;
